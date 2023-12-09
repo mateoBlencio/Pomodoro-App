00de015788fff4937409a9c6bc6a2aa416db265d
Mateo <90317@sistemas.frc.utn.edu.ar>
2023-12-09 11:30:40 -0300

    ringtone change

--- src/App.js
+++ src/App.js
@@ -1,23 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import useSound from "use-sound";
+import ring from "./ringtones/ringtone.mp4";
 
 function App() {
+  const [start, setStart] = useState(false);
+  const [seconds, setSeconds] = useState(0);
+  const [minutes, setMinutes] = useState(25);
+  const [brake, setBrake] = useState(false);
+  const [playRing] = useSound(ring);
+
+  useEffect(() => {
+    document.title = "Pomodoro-App";
+  }, []);
+
+  var timer;
+  useEffect(() => {
+    if (start === true) {
+      timer = setInterval(() => {
+        setSeconds(seconds - 1);
+
+        if (seconds === 0) {
+          setSeconds(59);
+          setMinutes(minutes - 1);
+
+          if (minutes === 0 && brake === false) {
+            playRing();
+            setBrake(true);
+            setMinutes(25);
+            setSeconds(0);
+          }
+
+          if (minutes === 0 && brake === true) {
+            playRing();
+            setBrake(false);
+            setMinutes(25);
+            setSeconds(0);
+          }
+        }
+      }, 1000);
+      return () => clearInterval(timer);
+    }
+  });
+
+  const startFunction = () => {
+    setStart(true);
+  };
+
+  const restartFunction = () => {
+    setMinutes(25);
+    setSeconds(0);
+    setStart(false);
+  };
+
+  const stopFunction = () => {
+    setStart(false);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <body className="App-header">
+        <h2>
+          {minutes < 10 ? "0" + minutes : minutes}:
+          {seconds < 10 ? "0" + seconds : seconds}
+        </h2>
+      </body>
+      <footer className="App-footer">
+        {start === false ? (
+          <button className="btn" onClick={startFunction}>
+            Start
+          </button>
+        ) : (
+          <button className="btn" onClick={stopFunction}>
+            Stop
+          </button>
+        )}
+        <button className="btn" onClick={restartFunction}>
+          Restart
+        </button>
+      </footer>
     </div>
   );
 }
